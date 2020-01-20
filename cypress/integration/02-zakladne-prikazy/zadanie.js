4e7ec322f00251b4df1be3d9ab8b24cad2a0e5fa
Filip Hric <filip@slido.com>
2020-01-20 23:53:52 +0100

    add more comments

--- cypress/integration/02-zakladne-prikazy/zadanie.js
+++ cypress/integration/02-zakladne-prikazy/zadanie.js
@@ -9,12 +9,14 @@ it('loads', () => {
 it('Has zero items', () => {
 
   // over, či sa v todo liste nachádza presne 0 položiek
+  // hint: použi príkaz .should()
   
 });
 
 it('Adds two items', () => {
 
   // pridaj do testu dve položky
+  // využi na to príkazy .get(), .click() a .type()
  
 });
 
@@ -29,6 +31,7 @@ it('Adds many items', () => {
 
 it('Marks item as completed and deletes item', () => {
 
-  // označ poloźku ako hotovú
+  // označ poloźku ako hotovú a následne ju vymaž
+  // pozor: na button „x“ pre vymazanie položky sa nedá kliknúť, pretože sa zobrazuje až na hoveri
   
 });
