af10a62e4ec5100e3c0a660c7b9aadd2c263e439
Filip Hric <filip@slido.com>
2020-02-03 20:48:12 +0100

    riešenie

--- cypress/integration/01-piseme-prvy-test/riesenie.js
+++ cypress/integration/01-piseme-prvy-test/riesenie.js
@@ -4,15 +4,12 @@ it('loads', () => {
     .visit('localhost:3000');
 
   cy
-    .log('heading is visible');
+    .screenshot();
 
+  cy
+    .log('heading is visible');
+    
   cy
     .contains('h1', 'todos');
 
-  cy
-    .contains('h1', /^todos$/);
-
-  cy
-    .contains('[data-cy=app-title]', 'todos');
-    
 });
